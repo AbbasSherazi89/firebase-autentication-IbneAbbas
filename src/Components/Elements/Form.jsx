433335c471aaf55cc0df8eb8e560bce7ec823903
ibne.abbassherazi89@gmail.com <ibne.abbassherazi89@gmail.com>
2022-11-11 15:24:12 +0500

    form styling

--- src/Components/Elements/Form.jsx
+++ src/Components/Elements/Form.jsx
@@ -1,56 +1,47 @@
 import React from "react";
 // import Box from '@mui/material/Box';
-import { TextField, Box } from "@mui/material";
-import { Container } from "react-bootstrap";
+import { TextField} from "@mui/material";
 import FormButton from "./FormButton";
 
 const Form = ({ title, setEmail, setPassword, handleAction }) => {
   return (
     <>
       <section className="signin-sec">
-        {/* // <Container style={{ maxwidth: "600px", margin: "8rem 16rem" }}> */}
-        <div className="pt-5">
-          <h2 className="text-center fw-bold">{title} Form</h2>
-        </div>
-        {/* <Box
-        component="form"
-        sx={{
-          "& > :not(style)": { m: 1, width: "35ch" },
-        }}
-        noValidate
-        autoComplete="off"
-      > */}
         <div className="container">
           <div className="row form-row">
-            <div className="col-md-4 form-colmn1">
-              <img className="img-fluid" src="assets/signup.svg" alt="" />
-            </div>
-            <div className="col-md-6 form-colmn2 py-5">
-              <TextField
-              className="mt-3"
-                id="email"
-                label="Email"
-                type="email"
-                variant="outlined"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <br />
-              <TextField
-                className="my-3"
-                id="password"
-                label="Password"
-                type="password"
-                variant="outlined"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <br />
-              <FormButton title={title} handleAction={handleAction} />
+            <div className="col-md-7 form-colmn">
+              <div className="row form-row2">
+                <div className="pt-4">
+                  <h2 className="text-center fw-bold"> {title} Form</h2>
+                </div>
+                <div className="col-md-4 form-colmn1">
+                  <img className="img-fluid" src="assets/signup.svg" alt="" />
+                </div>
+                <div className="col-md-6 form-colmn2 py-5">
+                  <TextField
+                    className="mt-3"
+                    id="email"
+                    label="Email"
+                    type="email"
+                    variant="outlined"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <br />
+                  <TextField
+                    className="my-3"
+                    id="password"
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <br />
+                  <FormButton title={title} handleAction={handleAction} />
+                </div>
+              </div>
             </div>
           </div>
         </div>
-
-        {/* </Box> */}
-        {/* </Container> */}
       </section>
     </>
   );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,45 +1,32 @@
 import React from "react";
 import { useEffect } from "react";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
 const Home = () => {
   const navigate = useNavigate();
 
-  useEffect
-    (() => {
-      let authToken = sessionStorage.getItem("auth");
-      if (authToken) {
-        navigate("/");
-      }
-      if (!authToken) {
-        navigate("/login");
-      }
-    },[navigate]);
+  useEffect(() => {
+    let authToken = sessionStorage.getItem("auth");
+    if (authToken) {
+      navigate("/");
+    }
+    if (!authToken) {
+      navigate("/login");
+    }
+  }, [navigate]);
 
-  const logout=()=>{
+  const logout = () => {
     sessionStorage.removeItem("auth");
-    navigate('/login');
-  }
+    navigate("/login");
+  };
   return (
     <>
-       <section className="signin-sec">
-        
+      <section className="signin-sec">
         <div className="pt-5">
           <h2 className="text-center fw-bold">Home</h2>
         </div>
-        
-        <div className="container">
-          <div className="row form-row">
-            <div className="col-md-3 form-colmn1">
-              
-            </div>
-            <div className="col-md-6 form-colmn2 py-5">
-            <Button onClick={logout}>Logout</Button>
-            </div>
-          </div>
-        </div>
+        <Button onClick={logout}>Logout</Button>
       </section>
-     
     </>
   );
 };
